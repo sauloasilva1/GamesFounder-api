8890547a3066a0b37e9b23fd9bd98b2e9778e5dd
saulo andre silva <sauloasilva@ig.com.br>
2020-10-31 16:05:30 -0300

    alteracaoMigrations

--- src/database/migrations/1601605300123-CriarUsuarios.ts
+++ src/database/migrations/1601605300123-CriarUsuarios.ts
@@ -1,7 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CriarUsuarios1601605300123 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "teste",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            isGenerated: true,
+          },
+        ],
+      })
+    );
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
